e44c8fbdbe634883eaa4c9af97a382c38e848857
Rob Kleffner <rob.kleffner@gmail.com>
2025-09-01 17:04:51 -0600

    Forgot to add a couple interfaces

--- access.go
+++ access.go
@@ -12,6 +12,11 @@ type DirectAccessLayerWriter interface {
 	SetFieldAt(backing io.WriteSeeker, coord SampleCoordinate, fieldIndex int, value any) error
 }
 
+type DirectAccessLayerReadWriter interface {
+	DirectAccessLayerReader
+	DirectAccessLayerWriter
+}
+
 type IterativeLayerReader interface {
 	NextField(backing io.ReadSeeker) (SampleCoordinate, any, error)
 	NextSample(backing io.ReadSeeker) (SampleCoordinate, []any, error)
@@ -22,4 +27,9 @@ type IterativeLayerWriter interface {
 	SetNextField(backing io.WriteSeeker, value any) error
 	SetNextSample(backing io.WriteSeeker, values []any) error
 	SetNamedNextSample(backing io.WriteSeeker, values map[string]any) error
+}
+
+type IterativeLayerReadWriter interface {
+	IterativeLayerReader
+	IterativeLayerWriter
 }

63454ce38106a1d4243bf13e14678ca79b329690
Rob Kleffner <rob.kleffner@gmail.com>
2025-09-01 18:18:47 -0600

    A bit more infrastructure

--- access.go
+++ access.go
@@ -2,12 +2,18 @@ package pixi
 
 import "io"
 
+type LayerExtension interface {
+	Layer() *Layer
+}
+
 type DirectAccessLayerReader interface {
+	LayerExtension
 	SampleAt(backing io.ReadSeeker, coord SampleCoordinate) ([]any, error)
 	FieldAt(backing io.ReadSeeker, coord SampleCoordinate, fieldIndex int) (any, error)
 }
 
 type DirectAccessLayerWriter interface {
+	LayerExtension
 	SetSampleAt(backing io.WriteSeeker, coord SampleCoordinate, values []any) error
 	SetFieldAt(backing io.WriteSeeker, coord SampleCoordinate, fieldIndex int, value any) error
 }
@@ -18,12 +24,14 @@ type DirectAccessLayerReadWriter interface {
 }
 
 type IterativeLayerReader interface {
+	LayerExtension
 	NextField(backing io.ReadSeeker) (SampleCoordinate, any, error)
 	NextSample(backing io.ReadSeeker) (SampleCoordinate, []any, error)
 	NamedNextSample(backing io.ReadSeeker) (SampleCoordinate, map[string]any, error)
 }
 
 type IterativeLayerWriter interface {
+	LayerExtension
 	SetNextField(backing io.WriteSeeker, value any) error
 	SetNextSample(backing io.WriteSeeker, values []any) error
 	SetNamedNextSample(backing io.WriteSeeker, values map[string]any) error
